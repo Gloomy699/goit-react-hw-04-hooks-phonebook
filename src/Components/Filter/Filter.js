5f68cd047d64135a9c00426a15dbbde4dcd5013b
Yaroslav Vasylenko <gloomy.699@gmail.com>
2020-12-20 23:09:23 +0200

    add hooks

---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <div className={s.findForm}>
+      <h2 className={s.title}>Поиск контактов:</h2>
+      <label className={s.label}>
+        <span className={s.labelTitle}>Имя:</span>
+        <input
+          type="text"
+          onChange={onChange}
+          value={value}
+          name="filter"
+          placeholder="введите имя"
+        />
+      </label>
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
